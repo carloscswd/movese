8913ad93dea70c66f8b661b0c60cc934718ecdfc
Carlos Silva <carloscwd@outlook.com>
2021-02-28 23:46:21 -0300

    Aplicação movit das aulas da Rockseat

---
+++ src/components/Profile.tsx
@@ -0,0 +1,20 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/7495561?s=460&u=da5ef658acdd4b4afcd3da8073c4326befaa31b1&v=4" alt="Carlos Silva" />
+            <div>
+                <strong>Carlos Silva</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
